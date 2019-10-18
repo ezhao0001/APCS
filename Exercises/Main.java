b774517d3c0f7969cada7770e827553f5a7bf4e6
Ellen Z <ezhao0001@gmail.com>
2019-10-18 08:28:47 -0700

    New exercise on loops

---
+++ Exercises/Main.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+import java.lang.Math;
+
+public class Main{
+  public static void main (String [] args){
+    
+   System.out.println("Enter two numbers: ");
+   
+   Scanner input = new Scanner (System.in);
+   int value1 = input.nextInt();
+   int value2 = input.nextInt();
+   
+   
+   while (value1){
+     
+   }
+   
+  }
+}   
+
+   
